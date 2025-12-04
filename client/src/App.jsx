cf8ea7f8b3612e9fdbb810542bb7ab0764c744a4
remanshu12 <remanshugoyal1@gmail.com>
2025-12-03 23:25:33 -0800

    made whiteboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Landing from './pages/landing'
 import Dashboard from './pages/Dashboard'
+import Whiteboard from './pages/Whiteboard'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/whiteboard" element={<Whiteboard />} />
       </Routes>
     </BrowserRouter>
   )
